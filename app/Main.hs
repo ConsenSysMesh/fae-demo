d7e6dc373b52bc88f13d9f534958606e39484bb0
therewillbecode <tomw08@gmail.com>
2018-04-11 21:24:33 +0100

    Add Aeson for JSON Operations

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,53 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
+import Prelude
 import Data.Char (isPunctuation, isSpace)
-import Data.Monoid (mappend)
+import Data.Monoid
 import Data.Text (Text)
 import Control.Exception (finally)
 import Control.Monad (forM_, forever)
 import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-
+import qualified Data.Text.Lazy.Encoding as D
+import qualified Data.Text.Lazy as X
 import qualified Network.WebSockets as WS
+import Data.Aeson
+import GHC.Generics
+
+data Bid = Bid {
+    value :: String
+  , bidder :: String
+  , timestamp :: String
+  } | Noop deriving (Show, Generic)
+  
+instance FromJSON Bid
+instance ToJSON Bid where
+    toJSON bid = toJSON $ show bid
 
 --We represent a client by their username and a `WS.Connection`. We will see how we
 --obtain this `WS.Connection` later on.
 
-type Client = (Text, WS.Connection)
+newtype Client = Client (Text, WS.Connection)
+
+instance Eq Client where
+  (Client (x, _)) == (Client (y, _)) = x == y
 
 --The state kept on the server is simply a list of connected clients. We've added
 --an alias and some utility functions, so it will be easier to extend this state
 --later on.
 
 type ServerState = [Client]
+
+instance ToJSON Client where
+    -- No need to provide a toJSON implementation.
+
+    -- For efficiency, we write a simple toEncoding implementation, as
+    -- the default version uses toJSON.
+    toJSON (Client (name, _)) = toJSON $ show (name, name)
 
 --Create a new, initial state:
 
@@ -35,7 +62,7 @@ numClients = length
 --Check if a user already exists (based on username):
 
 clientExists :: Client -> ServerState -> Bool
-clientExists client = any ((== fst client) . fst)
+clientExists client clients = elem client clients
 
 --Add a client (this does not check if the client already exists, you should do
 --this yourself using `clientExists`):
@@ -46,14 +73,15 @@ addClient client clients = client : clients
 --Remove a client:
 
 removeClient :: Client -> ServerState -> ServerState
-removeClient client = filter ((/= fst client) . fst)
+removeClient client = filter (/= client)
 
 --Send a message to all clients, and log it on stdout:
 
 broadcast :: Text -> ServerState -> IO ()
 broadcast message clients = do
     T.putStrLn message
-    forM_ clients $ \(_, conn) -> WS.sendTextData conn message
+    forM_ clients $ \(Client (_, conn)) -> WS.sendTextData conn message
+
 
 --The main function first creates a new state for the server, then spawns the
 --actual server. For this purpose, we use the simple server provided by
@@ -62,7 +90,7 @@ broadcast message clients = do
 main :: IO ()
 main = do
     state <- newMVar newServerState
-    WS.runServer "127.0.0.1" 9160 $ application state
+    WS.runServer "127.0.0.1" 9140 $ application state
 
 --Our main application has the type:
 
@@ -91,20 +119,20 @@ application state pending = do
 
 --Check that the first message has the right format:
 
-        _   | not (prefix `T.isPrefixOf` msg) ->
-                WS.sendTextData conn ("Wrong announcement" :: Text)
+     --   _   | not (prefix `T.isPrefixOf` msg) ->
+      --          WS.sendTextData conn ("Wrong announcement" :: Text)
 
 --Check the validity of the username:
 
-            | any ($ fst client)
-                [T.null, T.any isPunctuation, T.any isSpace] ->
-                    WS.sendTextData conn ("Name cannot " `mappend`
-                        "contain punctuation or whitespace, and " `mappend`
-                        "cannot be empty" :: Text)
+      --      | any ($ clientName)
+       --        [T.null, T.any isPunctuation, T.any isSpace] ->
+        --            WS.sendTextData conn ("Name cannot " `mappend`
+         --               "contain punctuation or whitespace, and " `mappend`
+          --              "cannot be empty" :: Text)
 
 --Check that the given username is not already taken:
 
-            | clientExists client clients ->
+         _  | clientExists client clients ->
                 WS.sendTextData conn ("User already exists" :: Text)
 
 --All is right! We're going to allow the client, but for safety reasons we *first*
@@ -118,26 +146,30 @@ application state pending = do
 
                modifyMVar_ state $ \s -> do
                    let s' = addClient client s
-                   WS.sendTextData conn $
-                       "Welcome! Users: " `mappend`
-                       T.intercalate ", " (map fst s)
-                   broadcast (fst client `mappend` " joined") s'
+                   T.putStrLn clientName
+                   print jsonClient
+                   WS.sendTextData conn $ T.pack "nnu"
+                   broadcast jsonBid s'
                    return s'
                talk conn state client
           where
             prefix     = "Hi! I am "
-            client     = (T.drop (T.length prefix) msg, conn)
+            clientName = T.filter  (\c -> c `notElem` ['"',' ']) $  T.drop (T.length prefix) msg
+            client     = Client (clientName, conn)
+            sampleBid  = Bid{ value="1", bidder="Xena", timestamp="12pm" }
+            jsonClient =  X.toStrict $ D.decodeUtf8 $ encode client
+            jsonBid = X.toStrict $ D.decodeUtf8 $ encode sampleBid
             disconnect = do
                 -- Remove client and return new state
                 s <- modifyMVar state $ \s ->
                     let s' = removeClient client s in return (s', s')
-                broadcast (fst client `mappend` " disconnected") s
+                broadcast (clientName `mappend` " disconnected") s
 
 --The talk function continues to read messages from a single client until he
 --disconnects. All messages are broadcasted to the other clients.
 
 talk :: WS.Connection -> MVar ServerState -> Client -> IO ()
-talk conn state (user, _) = forever $ do
+talk conn state (Client(user, _)) = forever $ do
     msg <- WS.receiveData conn
     readMVar state >>= broadcast
-        (user `mappend` ": " `mappend` msg)
+        (msg)
