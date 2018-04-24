39e16f7c9fba822a40cd00794bb84b537c8b8657
therewillbecode <tomw08@gmail.com>
2018-04-24 21:00:47 +0100

    Pattern Match on GetCoins Input

--- src/FaeTX/Outgoing/PostTX.hs
+++ src/FaeTX/Outgoing/PostTX.hs
@@ -32,9 +32,9 @@ getPostTXargs (CreateAuctionTXinput key aucTXID) =
 getPostTXargs (WithdrawCoinTXinput key aucTXID) =
   formatArgs [("key", show key), ("aucTX", show aucTXID)] ++ ["Withdraw"]
 getPostTXargs (GetCoinTXinput (Key (key))) =
-  formatArgs [("key", show key)] ++ ["GetCoin"]
-getPostTXargs (GetMoreCoinsTXinput key coinTXID) =
-  formatArgs [("key", show key), ("coinTX", show coinTXID)] ++ ["GetMoreCoins"]
+  formatArgs [("key", show key), ("self", show key)] ++ ["GetCoin"]
+getPostTXargs (GetMoreCoinsTXinput (Key (key)) (CoinTXID (coinTXID))) =
+  formatArgs [("key", show key),("self", show key), ("coinTX", show coinTXID)] ++ ["GetMoreCoins"]
 
 formatArg :: (String, String) -> [String]
 formatArg (key, val) = ["-e"] <> [key <> "=" <> val]
@@ -53,4 +53,4 @@ getFakeArg fake =
     else ""
 
 main :: IO ()
-main = postTX $ getPostTXargs (GetCoinTXinput (Key "tom"))
+main = postTX $ getPostTXargs (GetMoreCoinsTXinput (Key "tom") (CoinTXID "f6d974df9a7f2721f7f56f87d5cf6694d97c761df817ad6db47ccf91a960922b"))
