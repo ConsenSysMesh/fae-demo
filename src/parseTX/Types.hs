5022b3844a82880af25a73b9e719a5cba266884f
therewillbecode <tomw08@gmail.com>
2018-04-24 11:10:45 +0100

    More Faithful Fae TX Types

---
+++ src/parseTX/Types.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+type TXID = String
+
+type Key = String
+
+type CoinSCID = String
+
+type CoinVersion = String
+
+type CoinTXID = String
+
+type AucTXID = String
+
+data BidTXout = BidTXout
+  { txId :: TXID
+  , key :: Key
+  , aucTXID :: AucTXID
+  , coinTXID :: CoinTXID
+  , coinsSCID :: CoinSCID
+  , coinVersion :: CoinVersion
+  }
+
+data CreateAuctionTXout = CreateAuctionTXout
+  { txId :: TXID
+  , key :: Key
+  , aucTXID :: AucTXID
+  }
+
+data WithdrawCoinTXout = WithdrawCoinTXout
+  { txId :: TXID
+  , key :: Key
+  }
+
+data GetCoinTXout = GetCoinTXout
+  { txId :: TXID
+  , key :: Key
+  }
+
+data GetMoreCoinsTXout = GetMoreCoinsTXout
+  { txId :: TXID
+  , key :: Key
+  }
---
+++ src/postTX/Types.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+type Key = String -- private key for signing txs
+
+type CoinSCID = String -- hash of the coin
+
+type CoinVersion = String
+
+type CoinTXID = String -- id of tx which created coin
+
+type AucTXID = String -- id of the tx which created auction
+
+data FakeBidTXin = FakeBidTXinput
+  { key :: Key
+  , aucTXID :: AucTXID
+  , coinTXID :: CoinTXID
+  }
+
+data BidTXin = BidTXinput
+  { key :: Key
+  , aucTXID :: AucTXID
+  , coinTXID :: CoinTXID
+  , coinsSCID :: CoinSCID
+  , coinVersion :: CoinVersion
+  }
+
+data CreateAuctionTXin = CreateAuctionTXinput
+  { key :: Key
+  , aucTXID :: AucTXID
+  }
+
+data WithdrawCoinTXin = WithdrawCoinTXinput
+  { aucTXID :: AucTXID
+  , key :: Key
+  }
+
+data GetCoinTXin =
+  GetCoinTXinput Key
+
+data GetMoreCoinsTXin = GetMoreCoinsTXinput
+  { coinTXID :: CoinTXID
+  , key :: Key
+  }
