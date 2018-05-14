62a5d50ccd34303bc486c4328f2f5e4d0af2b9d0
therewillbecode <tomw08@gmail.com>
2018-05-14 11:06:21 +0100

    Increase Electron Window Size

--- electron/main.js
+++ electron/main.js
@@ -15,7 +15,7 @@ let faeserver
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 1024, height: 800 })
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/resources/client/index.html')
