746f829b43aee7a489a0baee76561a0eaff80fe3
therewillbecode <tomw08@gmail.com>
2018-06-02 15:41:27 +0100

    Run SocketAPI and UserAPI Servers Concurrently

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,11 @@ module Main where
 
 import API
 import Config
+import Control.Concurrent.Async
+import Control.Monad
 import Data.Aeson
 import qualified Data.ByteString.Char8 as C
+import Data.Foldable
 import Data.Text
 import Database
 import Network.Wai.Handler.Warp (run)
@@ -15,13 +18,15 @@ import Schema
 import Socket
 import System.Environment (lookupEnv)
 
-main :: IO ()
+main :: IO ((), ())
 main = do
   dbConnString <- getDBConnStrFromEnv
   userAPIPort <- getUserAPIPort defaultUserAPIPort
   socketAPIPort <- getSocketAPIPort defaultSocketAPIPort
+  let runSocketAPI = runSocketServer socketAPIPort dbConnString
+  let runUserAPI = run userAPIPort (app dbConnString)
   migrateDB dbConnString
-  run userAPIPort (app dbConnString)
+  concurrently runUserAPI runSocketAPI
   where
     defaultUserAPIPort = 8000
     defaultSocketAPIPort = 3000
--- src/Socket.hs
+++ src/Socket.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Socket
-  ( runServer
+  ( runSocketServer
   ) where
 
 import Config
@@ -24,17 +24,20 @@ import Socket.Types
 initialServerState :: ServerState
 initialServerState = ServerState {clients = M.empty, games = M.empty}
 
-runServer :: Int -> ConnectionString -> IO ()
-runServer port dbConnString = do
-  state <- newMVar initialServerState
-  WS.runServer "127.0.0.1" port $ application dbConnString state
+runSocketServer :: Int -> ConnectionString -> IO ()
+runSocketServer port dbConnString = do
+  print "d"
+  serverState <- newMVar initialServerState
+  print $ "Socket server listening on " ++ (show port :: String)
+  WS.runServer "127.0.0.1" port $ application dbConnString serverState
 
 -- New WS connections are expected to supply an access token as an initial msg
 -- Once the token is verified the connection only then will the server state be 
 -- updated with the newly authenticated client
 application :: ConnectionString -> MVar ServerState -> WS.ServerApp
-application dbConn state pending = do
+application dbConn serverState pending = do
   conn <- WS.acceptRequest pending
   WS.forkPingThread conn 30
   msg <- WS.receiveData conn
-  authClient state dbConn conn $ Token msg
+  print msg
+  authClient serverState dbConn conn $ Token msg
