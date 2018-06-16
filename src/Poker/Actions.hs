417a9da205b8de052c76dcab5235334d6837e2a8
therewillbecode <tomw08@gmail.com>
2018-06-16 23:12:16 +0100

    Add MakeBet

--- src/Poker/Actions.hs
+++ src/Poker/Actions.hs
@@ -26,14 +26,30 @@ import Prelude
 placeBet :: Int -> Player -> Player
 placeBet value = (chips -~ value) . (bet +~ value) . (committed +~ value)
 
+markActed = (actedThisTurn .~ True)
+
 makeBet :: Int -> PlayerName -> Game -> Game
-makeBet = undefined
+makeBet amount pName game@Game {..} =
+  ((players .~ newPlayers) . (currentPosToAct .~ nextPosToAct)) game
+  where
+    newPlayers =
+      (\p@Player {..} ->
+         if _playerName == pName
+           then (markActed . placeBet amount) p
+           else p) <$>
+      _players
+    nextPosToAct = incPosToAct game
 
 raise :: Int -> PlayerName -> Game -> Game
-raise = undefined
+raise amount playerName game@Game {..} = undefined
 
 fold :: PlayerName -> Game
 fold = undefined
 
 call :: PlayerName -> Game
 call = undefined
+
+incPosToAct :: Game -> Int
+incPosToAct Game {..} = _currentPosToAct `modInc` numActivePlayers
+  where
+    numActivePlayers = length $ getActivePlayers _players
