054eafa65571f46a8fce642f4d36052b596cf9cd
therewillbecode <tomw08@gmail.com>
2018-07-30 11:26:19 +0100

    Removed Duplication of Arbitrary Game

--- test/PokerSpec.hs
+++ test/PokerSpec.hs
@@ -13,74 +13,13 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Test.Hspec
 import Test.QuickCheck hiding (Big, Small)
-import Test.QuickCheck.Arbitrary.Generic
-import Test.QuickCheck.Gen
 
+import Arbitrary ()
 import Poker.ActionValidation
 import Poker.Game.Actions
 import Poker.Game.Game
 import Poker.Poker
 import Poker.Types
-
-instance Arbitrary Card where
-  arbitrary = genericArbitrary
-
-instance Arbitrary PlayerState where
-  arbitrary = genericArbitrary
-
-instance Arbitrary Street where
-  arbitrary = genericArbitrary
-
-instance Arbitrary Rank where
-  arbitrary = genericArbitrary
-
-instance Arbitrary Suit where
-  arbitrary = genericArbitrary
-
-instance Arbitrary HandRank where
-  arbitrary = genericArbitrary
-
--- this instance allows us to create random game values that can be used for property based testing
-instance Arbitrary Game where
-  arbitrary = do
-    _maxPlayers <- choose ((0, 10) :: (Integer, Integer))
-    let x = fromInteger _maxPlayers
-    noPlayers <- choose ((0, x) :: (Integer, Integer))
-    let z = fromInteger noPlayers
-    _players <- resize z arbitrary
-    _waitlist <- arbitrary
-    commSize <- choose ((0, 5) :: (Integer, Integer))
-    let y = fromInteger commSize
-    _board <- resize y arbitrary
-    _deck <- resize 52 arbitrary
-    _currentPosToAct <- arbitrary
-    _dealer <- choose (0, length _players)
-    _street <- arbitrary
-    _smallBlind <- suchThat chooseAny (>= 0)
-    let _bigBlind = _smallBlind * 2
-    _pot <- suchThat chooseAny (\x -> x >= 0 && x >= _bigBlind)
-    _maxBet <- suchThat chooseAny (>= 0)
-    let _winners = NoWinners
-    let _minBuyInChips = 1000
-    let _maxBuyInChips = 3000
-    return Game {_maxPlayers = fromInteger x, ..}
-
-instance Arbitrary Player where
-  arbitrary = do
-    _chips <- suchThat chooseAny (>= 0)
-    _committed <- suchThat chooseAny (>= 0)
-    _bet <-
-      suchThat chooseAny (\x -> (x >= 0) && x <= _chips && x <= _committed)
-    _playerName <- suchThat arbitrary (\n -> T.length n > 0)
-    _pockets <- suchThat arbitrary (\cards -> (null cards || length cards == 2))
-    _playerState <-
-      suchThat arbitrary (\s -> (s == None && (_committed > 0)) || s /= None)
-    _actedThisTurn <- arbitrary
-    return Player {..}
-
-instance Arbitrary Text where
-  arbitrary = T.pack <$> arbitrary
-  shrink xs = T.pack <$> shrink (T.unpack xs)
 
 player1 =
   Player
