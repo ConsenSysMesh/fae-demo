b4c0ded7b7a2ceb2ed6b45706122a24e0eaaaa7e
therewillbecode <tomw08@gmail.com>
2018-08-16 12:39:58 +0100

    Added Selectors for Game

--- client/app/containers/GameContainer.js
+++ client/app/containers/GameContainer.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { connect } from "react-redux";
 
 import Game from '../components/Game'
-import { getGame, isTurnToAct } from '../selectors/games'
+import { getGame, getPlayerPosition, isTurnToAct } from '../selectors/games'
 
 class GameContainer extends React.Component {
   render() {
     const { game, isTurnToAct, username } = this.props
-
+    console.log(this.props)
     return (<Game game={game} username={username} isTurnToAct={isTurnToAct} />)
   }
 }
 
 const mapStateToProps = (state, { username, match: { params: { tableName } } }) => ({
   game: getGame(tableName)(state),
-  isTurnToAct: isTurnToAct(username, tableName)(state)
+  isTurnToAct: isTurnToAct(username, tableName)(state),
+  playerPosition: getPlayerPosition(tableName)(state)
 });
 
 const mapDispatchToProps = (dispatch, { match: { params: { tableName } } }) => ({
--- client/app/selectors/tests/games.test.js
+++ client/app/selectors/tests/games.test.js
@@ -1,1 +1,9 @@
+/* eslint-disable */
 import Immutable from 'immutable'
+
+describe("Games Selectors", () => {
+  describe("getGames", () => {
+    it("should return correct action an authSuccess action for received asset", () => {
+      expect(authRequested()).toEqual({ type: types.AUTH_REQUESTED })
+    });
+  })
