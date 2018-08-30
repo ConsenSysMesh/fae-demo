0ba101a93a804ccf588c07908607dd6d3aea9ec2
therewillbecode <tomw08@gmail.com>
2018-08-30 13:17:59 +0100

    Use Modular Typographic Scale

--- client/app/components/Board.js
+++ client/app/components/Board.js
@@ -4,17 +4,19 @@ import Card from './Card';
 
 const Board = ({ cards }) => (
   <div className="board-cards">
-    {cards.map(card => {
-      console.log(card.toJS())
-      const rank = card.get('rank')
-      const suit = card.get('suit')
+    <div className='board-cards-container'>
+      {cards.map(card => {
+        console.log(card.toJS())
+        const rank = card.get('rank')
+        const suit = card.get('suit')
 
-      return (<Card
-        key={rank + suit}
-        rank={rank}
-        suit={suit}
-      />)
-    })}
+        return (<Card
+          key={rank + suit}
+          rank={rank}
+          suit={suit}
+        />)
+      })}
+    </div>
   </div>);
 
 export default Board;
