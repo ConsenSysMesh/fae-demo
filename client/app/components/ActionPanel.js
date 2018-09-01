c31cf1abc0ae4637a2cd0a5f362b4697049938a0
therewillbecode <tomw08@gmail.com>
2018-09-01 15:47:00 +0100

    Round Action Panel Edges on Large Screens

--- client/app/components/ActionPanel.js
+++ client/app/components/ActionPanel.js
@@ -33,70 +33,72 @@ const ActionPanel = ({
 }) => {
   console.log(userPocketCards)
 
-  return (<div className='actionPanel'>
-    <div className='action-panel-left-container'>
-      {userPocketCards ? userPocketCards.map(card => {
-        console.log(card.toJS())
-        const rank = card.get('rank')
-        const suit = card.get('suit')
+  return (
+    <div className='action-panel'>
+      <div className='action-panel-left-container'>
+        {userPocketCards ? userPocketCards.map(card => {
+          console.log(card.toJS())
+          const rank = card.get('rank')
+          const suit = card.get('suit')
 
-        return (<Card
-          key={rank + suit}
-          rank={rank}
-          suit={suit}
-        />)
-      }) : ''}
-    </div>
-    <div className='user-actions-container'>
-      <div>
-        <button
-          type="button"
-          onClick={() => postBigBlind()} className="button">
-          postBigBlind
-    </button>
-        <button
-          type="button" onClick={() => postSmallBlind()} className="button">
-          postSmallBlind
-    </button>
-        <button type="button" onClick={() => check()} className="button">
-          check
-    </button>
-        <button type="button" onClick={() => call()} className="button">
-          call</button>
-        <button
-          type="button"
-          onClick={() => bet(betValue)} className="button">Bet {betValue}</button>
-        <button
-          type="button"
-          onClick={() => raise(betValue)}
-          className="button">
-          Raise {betValue}</button>
-        <button
-          type="button"
-          onClick={() => fold()}
-          className="button">
-          Fold
-    </button>
-        <input
-          type="text"
-          value={betValue}
-          onChange={handleChange}
-        />
-        <button
-          type="button"
-          onClick={() => sitDown(betValue)}
-          className="button">
-          SitDown {betValue}
-        </button>
-        <button
-          type="button"
-          onClick={() => leaveGameSeat()}
-          className="button">
-          LeaveGame
-    </button>
+          return (<Card
+            key={rank + suit}
+            rank={rank}
+            suit={suit}
+          />)
+        }) : ''}
       </div>
-    </div>
-  </div>)
+      <div className='user-actions-container'>
+        <div>
+          <button
+            type="button"
+            onClick={() => postBigBlind()} className="button">
+            postBigBlind
+    </button>
+          <button
+            type="button" onClick={() => postSmallBlind()} className="button">
+            postSmallBlind
+    </button>
+          <button type="button" onClick={() => check()} className="button">
+            check
+    </button>
+          <button type="button" onClick={() => call()} className="button">
+            call</button>
+          <button
+            type="button"
+            onClick={() => bet(betValue)} className="button">Bet {betValue}</button>
+          <button
+            type="button"
+            onClick={() => raise(betValue)}
+            className="button">
+            Raise {betValue}</button>
+          <button
+            type="button"
+            onClick={() => fold()}
+            className="button">
+            Fold
+    </button>
+          <input
+            type="text"
+            value={betValue}
+            onChange={handleChange}
+          />
+          <button
+            type="button"
+            onClick={() => sitDown(betValue)}
+            className="button">
+            SitDown {betValue}
+          </button>
+          <button
+            type="button"
+            onClick={() => leaveGameSeat()}
+            className="button">
+            LeaveGame
+    </button>
+        </div>
+      </div>
+
+    </div>)
 }
 
 
