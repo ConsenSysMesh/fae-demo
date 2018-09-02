2dd5a1caadc24eb45a67e24962a8d814ccfe6226
therewillbecode <tomw08@gmail.com>
2018-09-02 12:56:36 +0100

    Use Gradient For Table

--- client/app/components/NavBar.js
+++ client/app/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = ({ isAuthenticated, username, history, logoutUser }) => (
     <div className="navbar-brand">
       <div>
         <a onClick={() => history.push('/')}>
-          <h4>Pokerspace</h4>
+          <h4>Gotham Poker</h4>
         </a>
       </div>
     </div>
