0c51f89fcf6509f2929b448e029504d4d0673280
therewillbecode <tomw08@gmail.com>
2018-09-25 19:56:35 +0100

    d

--- deploy-server.sh
+++ deploy-server.sh
@@ -6,5 +6,5 @@ sudo -H pip install pip==18.0.0 \
     && sudo -H pip uninstall --yes setuptools \
     && sudo -H pip install 'setuptools<20.2' \
     && sudo -H pip install 'requests[security]' \
-    && sudo -H pip install boto awscli ansible docker-py --upgrade \
+    && sudo -H pip install boto awscli ansible docker-py \
     && ansible-playbook server/ansible/push-new-image.yml server/ansible/deploy-image.yml
