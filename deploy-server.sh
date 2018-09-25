2d64a7a153bc75922eeec035194a000e7555d6e0
therewillbecode <tomw08@gmail.com>
2018-09-25 14:28:08 +0100

    d

---
+++ deploy-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## Build and push new docker image to AWS ECR then pull and run new image in EC2 instance.
+sudo -H pip install pip==18.0.0 \
+    && sudo -H pip uninstall --yes setuptools \
+    && sudo -H pip install 'setuptools<20.2' \
+    && sudo -H pip install 'requests[security]' \
+    && sudo -H pip install boto awscli ansible docker-py --upgrade \
+    && ansible-playbook server/ansible/push-new-image.yml server/ansible/deploy-image.yml
