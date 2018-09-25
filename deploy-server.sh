b3d443d7afde4a4d7c03a2491c2db1775647dac3
therewillbecode <tomw08@gmail.com>
2018-09-25 14:57:37 +0100

    d

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ## Build and push new docker image to AWS ECR then pull and run new image in EC2 instance.
+## Ensure AWS credentials are set in environment or the ansible playbook will fail to login to AWS
+
 sudo -H pip install pip==18.0.0 \
     && sudo -H pip uninstall --yes setuptools \
     && sudo -H pip install 'setuptools<20.2' \
