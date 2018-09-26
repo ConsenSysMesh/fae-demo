a6ec36512384bf1a25765199e17d19973e803bfc
therewillbecode <tomw08@gmail.com>
2018-09-26 11:55:08 +0100

    d

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+## First arg to the bashfile must be the path to the key file to SSH into the EC2 host(s)
+##
 ## Build and push new docker image to AWS ECR then pull and run new image in EC2 instance.
 ## Ensure AWS credentials are set in environment or the ansible playbook will fail to login to AWS
+
+if [ $# -eq 0 ]
+  then
+    echo "You forgot to speciffy the path to the key file to authenticate the SSH connection"
+fi
 
 sudo -H pip install pip==18.0.0 \
     && sudo -H pip uninstall --yes setuptools \
     && sudo -H pip install 'setuptools<20.2' --ignore-installed \
     && sudo -H pip install 'requests[security]' --ignore-installed \
     && sudo -H pip install boto awscli ansible docker-py --ignore-installed \
-    && ansible-playbook ansible/push-new-image.yml ansible/deploy-image.yml
+    && ANSIBLE_CONFIG=server/ansible/ansible2.cfg ansible-playbook server/ansible/deploy-image.yml --key-file=$1
