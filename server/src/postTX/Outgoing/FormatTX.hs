211601f125cfa44d98f4f7e275588075454131f3
therewillbecode <tomw08@gmail.com>
2018-11-01 15:54:41 +0000

    Create Record Type for PostTX Options

--- server/src/postTX/Outgoing/FormatTX.hs
+++ server/src/postTX/Outgoing/FormatTX.hs
@@ -15,22 +15,28 @@ import PostTX.Types
 import SharedTypes 
 import PostTX.Outgoing.Types
 
+type ContractName = String
 type Env = [(String, String)]
 type Args = [String]
 
+data PostTXOpts = PostTXOpts { 
+  contractName :: String,
+  env :: [(String, String)],
+  args :: [String]
+}
+
 -- Get the necessary environment variables and args for PostTX in order
 -- to post the auction TX
-getPostTXopts :: AuctionTXin -> (Env, Args)
-getPostTXopts (FakeBidTXin (Key key) (AucTXID aucTXID) (CoinTXID coinTXID)) = (env, ["Bid", "--fake"])
+getPostTXopts :: AuctionTXin -> (ContractName, Env, Args)
+getPostTXopts (FakeBidTXin (Key key) (AucTXID aucTXID) (CoinTXID coinTXID)) = ("Bid", env, ["--fake"])
   where env = [("key", key), ("aucTX", aucTXID), ("coinTX", coinTXID), ("coinSCID", ""), ("coinVersion", "")]
-getPostTXopts (BidTXin (Key key) (AucTXID aucTXID) (CoinTXID coinTXID) (CoinSCID coinSCID) (CoinVersion coinVersion)) = (env, ["Bid"])
+getPostTXopts (BidTXin (Key key) (AucTXID aucTXID) (CoinTXID coinTXID) (CoinSCID coinSCID) (CoinVersion coinVersion)) = ("Bid", env, [])
   where env = [ ("key", key), ("aucTX", aucTXID), ("coinTX", coinTXID), ("coinSCID", coinSCID), ("coinVersion", coinVersion) ]
-getPostTXopts (CreateAuctionTXin (Key key)) = (env, ["Create"])
+getPostTXopts (CreateAuctionTXin (Key key)) = ("Create", env, [])
   where env = [("key", key)] 
-getPostTXopts (WithdrawTXin (Key key) (AucTXID aucTXID)) = (env, ["Withdraw"])
+getPostTXopts (WithdrawTXin (Key key) (AucTXID aucTXID)) = ("Withdraw", env, [])
   where env = [("key", key), ("aucTX", aucTXID)]
-getPostTXopts (GetCoinTXin (Key key)) = (env, ["GetCoin"])
+getPostTXopts (GetCoinTXin (Key key)) = ("GetCoin", env, [])
   where env = [("key", key), ("self", key)] 
-getPostTXopts (GetMoreCoinsTXin (Key key) (CoinTXID coinTXID)) = (env, ["GetMoreCoins"])
+getPostTXopts (GetMoreCoinsTXin (Key key) (CoinTXID coinTXID)) = ("GetMoreCoins", env, [])
   where env = [("self", "bidder1"), ("key", "bidder1"), ("coinTX", coinTXID)]
-getPostTXopts d = traceShow d ([],[]) 
